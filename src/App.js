ae49f6b54a6554cc8414617790cebe9546234c67
AriellaRogueN <arrodriguezn@gmail.com>
2018-03-06 17:08:05 -0300

    Agregando cambios

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import LocationList from './components/LocationList';  //reemplazo WeatherLocation
 
+
+const cities = [
+  'Santiago,scl',
+  'Buenos Aires,ar',
+  'Bogotá,col',
+  'Ciudad de México,mx',
+]
+
+
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
       <div className="App">
-          <LocationList></LocationList>   
+          <LocationList cities={cities}></LocationList>   
       </div>
       </MuiThemeProvider>
       
--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import Proptypes from 'prop-types'
 import WeatherLocation from './WeatherLocation';
+//Modificamos para pasar ciudades
+const strToComponent =  cities => (
+    cities.map(city => (<WeatherLocation city={city}/>))
+)          //pasamos cities a weatherlocation
 
-const LocationList = () => (
+const LocationList = ({cities}) => (
     <div>
-        <WeatherLocation city = {'Santiago,scl'}/>
+        {strToComponent(cities)}
+    </div>
+)
+
+/*const LocationList = (cities) => (
+    <div>
+        <WeatherLocation city= {'Santiago,scl'}/>
         <WeatherLocation city={'Bogota,col'}/>
         <WeatherLocation city={'Buenos Aires,ar'}/>
         <WeatherLocation city={'Rio de Janeiro,br'}/>
-    </div>
-)
+    </div>  antes de pasar map,esto fue lo que cambio*/ 
+
+LocationList.propTypes ={
+    cities: Proptypes.array.isRequired    //Validemos que este en un array
+}
 export default LocationList;
 
